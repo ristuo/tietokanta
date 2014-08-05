9b595a9ce30ca16ee1091cde25782d89037f9ced
risto <risto@localhost.home>
2014-08-05 21:23:40 +0300

    vähän käliä

---
+++ html-demo/navigaatio.php
@@ -0,0 +1,6 @@
+        <ul class="nav nav-tabs">
+            <li class="active"> <a href="etusivu.php"> etusivu </a></li>
+            <li class="active"> <a href="kirjautuminen.php"> kirjaudu </a></li>
+            <li class="active"> <a href="kilpailut.php"> kilpailut </a></li>
+        
+        </ul>
