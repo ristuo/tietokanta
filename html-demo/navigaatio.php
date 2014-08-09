ca76b4f01f958ab04f040b8da1f9fc8a8e231e09
risto <risto@localhost.home>
2014-08-09 19:59:14 +0300

    turhien poisto

--- html-demo/navigaatio.php
+++
@@ -1,6 +0,0 @@
-        <ul class="nav nav-tabs">
-            <li class="active"> <a href="etusivu.php"> etusivu </a></li>
-            <li class="active"> <a href="kirjautuminen.php"> kirjaudu </a></li>
-            <li class="active"> <a href="kilpailut.php"> kilpailut </a></li>
-        
-        </ul>
