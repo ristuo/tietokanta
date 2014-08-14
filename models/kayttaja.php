462a4b5014b44c34a9bf068b721ac63bef63ecf0
risto <risto@localhost.home>
2014-08-15 00:54:02 +0300

    mallit

--- models/kayttaja.php
+++ models/kayttaja.php
@@ -1,13 +1,39 @@
 <?php
 
+require 'libs/tietokantayhteys.php';
+
+
 class kayttaja {
-    private $id;
-    private $tunnus;
+    private $knro;
+    private $sposti;
     private $salasana;
     
-    public function __construct($id, $tunnus, $salasana) {
-        $this->tunnus = $tunnus;
-        $this->id = $id;
+    public function __construct($knro, $sposti, $salasana) {
+        $this->sposti = $sposti;
+        $this->knro = $knro;
         $this->salasana = $salasana;
+    }
+    
+    
+    public static function haeKayttajaTunnuksilla($kayttaja, $passu) {
+        
+        
+        
+        $sql = 'SELECT knro, sposti, salasana FROM kayttaja WHERE sposti = ? AND salasana = ? LIMIT 1';
+        $kysely = getTietokantayhteys()->prepare($sql);
+        $kysely->execute(array($kayttaja, $passu));
+        $tulos = $kysely->fetchObject();
+        
+        
+        if ($tulos == null) {
+            return null;
+        }
+        else {
+            $id = $tulos->knro;
+            $nimi = $tulos->sposti;
+            $salasana = $tulos->salasana;
+            $palautettava = new kayttaja($id, $nimi, $salasana);
+            return $palautettava;
+        }
     }
 }
--- models/nostaja.php
+++ models/nostaja.php
@@ -1,9 +1,31 @@
 <?php
-require 'tietokantayhteys.php';
-$sql = "select * from nostaja";
-$kysely = getTietokantayhteys()->prepare($sql);
-$kysely->execute();
+require 'libs/tietokantayhteys.php';
 
-function haeRivi() {
-    return $kysely->fetch();
+
+function haeKaikkiNostajat() {
+    $sql = "select nimi, kansalaisuus, seura, extract(year from syntymapaiva) as syntymavuosi from nostaja";
+    return toteutaKysely($sql);
+}
+
+function toteutaKysely($haku) {
+    $kysely = getTietokantayhteys()->prepare($haku);
+    $kysely->execute();
+    return $kysely->fetchAll(PDO::FETCH_OBJ);
+}
+
+function haeSukupuolenPerusteella($sukupuoli) {
+    $sql = "select hnro, nimi, kansalaisuus, seura, extract(year from syntymapaiva) as syntymavuosi from nostaja where sukupuoli = '".$sukupuoli."'";
+    return toteutaKysely($sql);
+    
+}
+
+function haeNostajaNumerolla($hnro) {
+    $sql = "select hnro, nimi, kansalaisuus, seura, extract(year from syntymapaiva) as syntymavuosi, sukupuoli from nostaja where hnro = ".$hnro;
+    return toteutaKysely($sql);
+    
+}
+
+function haeNostajanNostotNumerolla($hnro) {
+    $sql = "select * from nosto where hnro = ".$hnro;
+    return toteutaKysely($sql);
 }
